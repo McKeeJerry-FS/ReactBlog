d5482ef66235935271a1ce05dc647e6c2c937218
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-03-30 13:16:53 -0400

    Fixed a failure point with test

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -1,6 +1,10 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
 export default async function globalSetup() {
+  if (process.platform === 'linux' && !process.env.MONGOMS_DISTRO) {
+    process.env.MONGOMS_DISTRO = 'ubuntu-22.04'
+  }
+
   const instance = await MongoMemoryServer.create({
     binary: {
       version: '7.0.14',
