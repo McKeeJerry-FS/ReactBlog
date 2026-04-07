6f1aff6249cba977ee239d5757465c7eb20a5b67
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-04-07 10:04:26 -0400

    chore: implementing data aggregation

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,4 +1,10 @@
-import { trackEvent } from '../services/events.js';
+import { 
+    trackEvent,
+    getTotalViews,
+    getDailyViews,
+    getDailyDurations, 
+} from '../services/events.js';
+
 import { getPostById } from '../services/posts.js';
 
 export function eventRoutes(app) {
@@ -14,5 +20,44 @@ export function eventRoutes(app) {
         } catch (error) {
             res.status(500).json({ error: 'Internal server error' });
         }
+    })
+
+    app.get('/api/v1/events/totalViews/:postId', async (req, res) => {
+        try {
+            const { postId } = req.params;
+            const post = await getPostById(postId);
+            if ( post === null) return res.status(400).end()
+            const stats = await getTotalViews(postId);
+            return res.json(stats);    
+        } catch (error) {
+            console.error('Error getting stats:', error);
+            return res.status(500).end()
+        }
+    })
+
+    app.get('/api/v1/events/dailyViews/:postId', async (req, res) => {
+        try {
+            const { postId } = req.params;
+            const post = await getPostById(postId);
+            if ( post === null) return res.status(400).end()
+            const stats = await getDailyViews(postId);
+            return res.json(stats);    
+        } catch (error) {
+            console.error('Error getting stats:', error);
+            return res.status(500).end()
+        }   
+    })
+
+    app.get('/api/v1/events/dailyDurations/:postId', async (req, res) => {
+        try {
+            const { postId } = req.params;
+            const post = await getPostById(postId);
+            if ( post === null) return res.status(400).end()
+            const stats = await getDailyDurations(postId);
+            return res.json(stats);    
+        } catch (error) {
+            console.error('Error getting stats:', error);
+            return res.status(500).end()
+        }   
     })
 }
